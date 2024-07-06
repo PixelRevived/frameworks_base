b094ec37ce74045ad59abdf86e4586f2ed975905
Hridaya Prajapati <hridaya@pixelos.net>
2024-07-06 14:47:32 +0200

    Revert "DeviceConfigUtils: Add support for keep property's values"
    This reverts commit 9634106776db7f875d6ccaf3bf1a24f70eb93d8e.
    
    Change-Id: I585cc0dc8965158f79c003fa2f9587dfb26d1eda

--- core/java/com/android/internal/util/custom/DeviceConfigUtils.java
+++ core/java/com/android/internal/util/custom/DeviceConfigUtils.java
@@ -17,14 +17,10 @@
 package com.android.internal.util.custom;
 
 import android.content.res.Resources;
-import android.content.ContentResolver;
 import android.provider.Settings;
-import android.text.TextUtils;
 import android.util.Log;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -46,77 +42,23 @@ public class DeviceConfigUtils {
         return allDeviceConfigs;
     }
 
-    private static String[] getDeviceConfigsKeep() {
-        return Resources.getSystem()
-                    .getStringArray(com.android.internal.R.array.device_configs_propertyToKeep);
-    }
-
-    private static List<String> getAllowedPackages() {
-        return Arrays.asList(Resources.getSystem()
-                    .getStringArray(com.android.internal.R.array.device_configs_modifyAllowedPackages));
-    }
-
-    private static boolean shouldKeepProperty(String namespace, String property) {
-        for (String p : getDeviceConfigsKeep()) {
-            String[] kv = p.split("=");
-            String fullKey = kv[0];
-            String[] nsKey = fullKey.split("/");
-
-            if (nsKey[0].equals(namespace) && nsKey[1].equals(property)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public static Map<String, String> filterKeepValue(String namespace, Map<String, String> keyValues) {
-        for (String p : getDeviceConfigsKeep()) {
-            String[] kv = p.split("=");
-            String fullKey = kv[0];
-            String[] nsKey = fullKey.split("/");
-
-            if (!nsKey[0].equals(namespace)) {
-                continue;
-            }
-            if (keyValues.containsKey(nsKey[1])) {
-                keyValues.remove(nsKey[1]);
-            }
-        }
-
-        return keyValues;
-    }
-
-    public static boolean shouldDenyDeviceConfigControl(
-            String packageName, String namespace, String property) {
-        if (!TextUtils.isEmpty(packageName)
-                && getAllowedPackages().contains(packageName)) {
-            logd("shouldAllowDeviceConfigControl, allow, package=" + packageName + ", namespace=" + namespace + ", property=" + property);
-            return false;
-        }
-
-        logd("shouldAllowDeviceConfigControl, package=" + packageName + ", namespace=" + namespace + ", property=" + property);
-
-        if (shouldKeepProperty(namespace, property)) {
-            logd("shouldAllowDeviceConfigControl, deny, package=" + packageName + ", namespace=" + namespace + ", property=" + property);
-            return true;
-        }
-
+    public static boolean shouldDenyDeviceConfigControl(String namespace, String property) {
+        if (DEBUG) Log.d(TAG, "shouldAllowDeviceConfigControl, namespace=" + namespace + ", property=" + property);
         for (String p : getDeviceConfigsOverride()) {
             String[] kv = p.split("=");
             String fullKey = kv[0];
             String[] nsKey = fullKey.split("/");
-            if (nsKey[0].equals(namespace) && nsKey[1].equals(property)) {
-                logd("shouldAllowDeviceConfigControl, deny, package=" + packageName + ", namespace=" + namespace + ", property=" + property);
+            if (nsKey[0] == namespace && nsKey[1] == property){
+                if (DEBUG) Log.d(TAG, "shouldAllowDeviceConfigControl, deny, namespace=" + namespace + ", property=" + property);
                 return true;
             }
         }
-        logd("shouldAllowDeviceConfigControl, allow, package=" + packageName + ", namespace=" + namespace + ", property=" + property);
+        if (DEBUG) Log.d(TAG, "shouldAllowDeviceConfigControl, allow, namespace=" + namespace + ", property=" + property);
         return false;
     }
 
     public static void setDefaultProperties(String filterNamespace, String filterProperty) {
-        logd("setDefaultProperties");
+        if (DEBUG) Log.d(TAG, "setDefaultProperties");
         for (String p : getDeviceConfigsOverride()) {
             String[] kv = p.split("=");
             String fullKey = kv[0];
@@ -125,11 +67,11 @@ public class DeviceConfigUtils {
             String namespace = nsKey[0];
             String key = nsKey[1];
 
-            if (filterNamespace != null && namespace.equals(filterNamespace)) {
+            if (filterNamespace != null && filterNamespace == namespace){
                 continue;
             }
 
-            if (filterProperty != null && key.equals(filterProperty)) {
+            if (filterProperty != null && filterProperty == key){
                 continue;
             }
 
@@ -139,9 +81,5 @@ public class DeviceConfigUtils {
             }
             Settings.Config.putString(namespace, key, value, false);
         }
-    }
-
-    private static void logd(String msg) {
-        if (DEBUG) Log.d(TAG, msg);
     }
 }
